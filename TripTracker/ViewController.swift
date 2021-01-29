7a76d81d79e4f0e23976e3db4b4f7590c949e4ac
Amber Spadafora <ahelenespadafora@gmail.com>
2021-01-29 18:00:47 -0500

    Initial Commit

---
+++ TripTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TripTracker
+//
+//  Created by Amber Spadafora on 1/29/21.
+//  Copyright © 2021 Amber Spadafora. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
