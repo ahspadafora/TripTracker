839588041aadff6f65cef75b4b014ff3b4691c6a
Amber Spadafora <ahelenespadafora@gmail.com>
2021-01-31 15:38:42 -0500

    add trips to coreData and fetch trips from coredata

---
+++ TripTracker/Managers/CoreDataStack.swift
@@ -0,0 +1,70 @@
+//
+//  CoreDataStack.swift
+//  TripTracker
+//
+//  Created by Amber Spadafora on 1/31/21.
+//  Copyright © 2021 Amber Spadafora. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class CoreDataStack {
+    
+    public static let modelName = "TripTracker"
+    static let model: NSManagedObjectModel = {
+        guard let modelURL = Bundle.main.url(forResource: modelName, withExtension: "momd") else {
+            return NSManagedObjectModel()
+        }
+        return NSManagedObjectModel(contentsOf: modelURL) ?? NSManagedObjectModel()
+    }()
+    
+    init(){}
+    
+    public lazy var mainContext: NSManagedObjectContext = {
+        return storeContainer.viewContext
+    }()
+    
+    public lazy var storeContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: CoreDataStack.modelName, managedObjectModel: CoreDataStack.model)
+        container.loadPersistentStores { _, error in
+            if let error = error {
+                fatalError("Unresolved error \(error), \(error.localizedDescription)")
+            }
+        }
+        return container
+    }()
+    
+    // for Unit Testing
+    func newDerivedContext() -> NSManagedObjectContext {
+        let context = storeContainer.newBackgroundContext()
+        return context
+    }
+    
+    func saveContext(context: NSManagedObjectContext){
+        if context != mainContext {
+            saveDerivedContext(context)
+            return
+        }
+        
+        context.perform {
+            do {
+                try context.save()
+            } catch let error {
+                print(error.localizedDescription)
+            }
+        }
+    }
+    
+    public func saveDerivedContext(_ context: NSManagedObjectContext) {
+      context.perform {
+        do {
+          try context.save()
+        } catch let error as NSError {
+          fatalError("Unresolved error \(error), \(error.userInfo)")
+        }
+
+        self.saveContext(context: self.mainContext)
+      }
+    }
+}
--- TripTracker/ViewControllers/PastTripsViewController.swift
+++ TripTracker/ViewControllers/PastTripsViewController.swift
@@ -11,6 +11,9 @@ import UIKit
 class PastTripsViewController: UIViewController {
     
     var locationManager: LocationProvider = LocationManager()
+    var tripService: TripService?
+    
+    
     
     // past trips table view datasource & delegate
     let tableViewDelegate = PastTripsTableViewDelegate()
@@ -20,9 +23,17 @@ class PastTripsViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableViewDataSource.tripService = self.tripService
+        
         pastTripsTableView.delegate = tableViewDelegate
         pastTripsTableView.dataSource = tableViewDataSource
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        self.pastTripsTableView.reloadData()
+    }
+    
 }
 
 class PastTripsTableViewDelegate: NSObject, UITableViewDelegate {
@@ -30,16 +41,26 @@ class PastTripsTableViewDelegate: NSObject, UITableViewDelegate {
 }
 
 class PastTripsTableViewDataSource: NSObject, UITableViewDataSource {
-    let numberOfRows = 5
+    
+    var trips: [Trip]? = []
+    var tripService: TripService? {
+        didSet {
+            let tripCount = tripService?.getTrips()
+            print("trip count at setting tripService is = \(tripCount?.count)")
+        }
+    }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return numberOfRows
+        
+        return tripService?.getTrips()?.count ?? 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "pastTripCell", for: indexPath) as? PastTripTableViewCell else { return UITableViewCell() }
         return cell
     }
+    
+    
     
     
 }
